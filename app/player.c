a50daecb0c98bd358798f7b1f0bf98a60fdba6bf
GJP1106 <GJP1106@outlook.com>
2019-04-27 11:52:12 +0800

    3.9、支持使用第三方的静态库和动态库

--- app/player.c
+++ app/player.c
@@ -4,11 +4,15 @@
 #include "usb.h"
 #include "media.h"
 #include "math.h"
+#include "jpg.h"
+#include "rmvb.h"
 int main() {
 	printf("MP3 player init....\n");
 	lcd_init();
 	usb_init();
 	media_init();
 	math_dll_init();
+	jpg_init();
+	rmvb_init();
 	return 0;
 }
